28e437381280c05ba2b4733c8851ccb8fe8d7ef1
bohyeon <ktseo41@gmail.com>
2022-05-15 22:43:28 +0900

    modify: build 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ktseo41/quotes-sharer.git main:gh-pages
+git push -f https://github.com/ktseo41/quotes-sharer main:gh-pages
 
 cd -
